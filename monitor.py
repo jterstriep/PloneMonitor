7655d6de197dc853d1fdaa4f678f9188d275b61c
Darkhan Kubigenov <darkhanu@gmail.com>
2013-11-17 22:07:21 -0600

    moving back to python2

--- monitor.py
+++ monitor.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python2
 
 
 """
@@ -7,8 +7,8 @@ argv[1] location of the plone site
 argv[2] (optional) - poll vs responding to request (not implemented)
 """
 
-from urllib.parse import urljoin
-import urllib.request
+from urlparse import urljoin, urlparse
+import urllib
 import json
 import sys, subprocess, shlex
 
@@ -17,17 +17,20 @@ service_url = "@@get_next_job"
 def main():
     arg_count = len(sys.argv)
     if arg_count < 2:
-        print("Error: you need to specify location address of the plone site")
-        print("Exiting script")
+        print "Error: you need to specify location address of the plone site"
+        print "Exiting script"
         return 1
     location = sys.argv[1]
+    if not location.startswith('http'):
+        location = '%s%s' % ('http://', location)
     ## using hash value for testing
     ## later hash value needs to be passed in by plone site
     full_url = urljoin(location, service_url + '?hash=12345')
     try:
-        response = urllib.request.urlopen(full_url)
+        print full_url
+        response = urllib.urlopen(full_url)
         pretty = json.loads(response.read().decode())
-        print(pretty)
+        print pretty
         (status_ok, start_string) = parse_response(pretty)
         if status_ok:
             args = shlex.split(start_string)
@@ -36,25 +39,25 @@ def main():
                 p.wait()
                 return 0
             except OSError:
-                print('Failed while trying to run ' + start_string)
+                print 'Failed while trying to run ', start_string
 
         else:
-            print('Status wasn\'t OK. Exiting')
+            print 'Status wasn\'t OK. Exiting'
             return 1
-    except URLError:
-        print ('Could not connect to server')
+    except Exception:
+        print 'Could not connect to server'
         return 1
 
 def parse_response(pretty):
     if not 'response' in pretty:
-        print('Wrong response from server')
+        print 'Wrong response from server'
         return (False,'')
     response_status = pretty['response']
     if not response_status == 'OK':
-        print('Response is NOTOK')
+        print 'Response is NOTOK'
         return (False,'')
     if not 'start_string' in pretty:
-        print('No start string is found')
+        print 'No start string is found'
         return (False,'')
     return (True,pretty['start_string'])
 
