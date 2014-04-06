f4bebf91ec38ca1cce4110613949a93dd91a87d4
Darkhan Kubigenov <darkhanu@gmail.com>
2014-04-05 20:21:08 -0500

    implementing update_status and get_status

--- monitor.py
+++ monitor.py
@@ -3,21 +3,28 @@
 
 """
 argv[0] is name of script
-argv[1] location of the plone site
+argv[1] location of the vm
 argv[2] access_key
 """
 
 from urlparse import urljoin
-from sys import argv
 import urllib2
-import subprocess, shlex, logging, time, json
+import urllib.urlencode
+import subprocess
+import shlex
+import logging
+import time
+import json
 from argparse import ArgumentParser
 
-SERVICE_URL = "@@get_next_job"
-SERVER_POLL_DELAY = 3 # seconds
+NEXT_JOB_SERVICE_URL = 'get_next_job'
+GET_STATUS_URL = 'provide_status'
+UPDATE_STATUS_URL = 'update_status'
+SERVER_POLL_DELAY = 3   # seconds
 
 # enable logging
 logging.basicConfig(level=logging.DEBUG)
+
 
 def main():
     """ The main function """
@@ -25,24 +32,25 @@ def main():
     global SERVER_POLL_DELAY
     # parse the command line arguments
     parser = ArgumentParser(description='Execute shell script from plone site')
-    parser.add_argument('site_url')
+    parser.add_argument('vm_url')
     parser.add_argument('access_key')
     parser.add_argument('--polling_delay', default=SERVER_POLL_DELAY)
 
     args = vars(parser.parse_args())
-
+    vm_url = args['vm_url']
+    if not vm_url.startswith('http'):
+        vm_url = '%s%s' % ('http://', vm_url)
     SERVER_POLL_DELAY = args['polling_delay']
 
-
     while True:
-        response = get_next_job(args['site_url'], args['access_key'])
+        response = make_request(vm_url, NEXT_JOB_SERVICE_URL, args['access_key'])
 
         logging.info('Got response: %s', response)
 
         pretty = json.loads(response)
         (status_ok, start_string) = parse_response(pretty)
         if status_ok:
-            execute_job(start_string)
+            execute_job(start_string, args['vm_url'])
         else:
             break
         time.sleep(SERVER_POLL_DELAY)
@@ -52,20 +60,20 @@ def main():
     # posssibly shut down the machine here
     return 1
 
-def get_next_job(site_url, access_key):
+
+def make_request(vm_url, service_url, access_key, params):
     """ get the next job from plone """
-    if not site_url.startswith('http'):
-        site_url = '%s%s' % ('http://', site_url)
 
-    logging.info('siteurl: %s access_key: %s', site_url, access_key)
+    logging.info('siteurl: %s access_key: %s', vm_url, access_key)
 
-    endpoint = urljoin(site_url, SERVICE_URL)
+    endpoint = urljoin(vm_url, service_url)
 
-    endpoint = urljoin(endpoint, '?hash=', access_key)
+    endpoint = urljoin(endpoint, '?hash=%s' % access_key)
     logging.info('endpoint: %s', endpoint)
 
     request = urllib2.Request(endpoint)
-
+    if params:
+        request.add_data(urllib.urlencode(params))
     # add authentication header
     # request.add_header("Authorization", "Basic %s" % userpass)
 
@@ -75,8 +83,8 @@ def get_next_job(site_url, access_key):
         return response.read()
 
     except urllib2.HTTPError as herror:
-        logging.error('Received error code ' + herror.code)
-        return 0
+        logging.error('Received error code ' + str(herror.code))
+        return None
     except urllib2.URLError:
         logging.error('Could not connect to server')
         return None
@@ -100,7 +108,7 @@ def parse_response(pretty):
     return (True, pretty['start_string'])
 
 
-def execute_job(start_string):
+def execute_job(start_string, vm_url, access_key):
     """ Run the specified shell command
         in a new process.
         Polls the plone site about the state of the job
@@ -127,15 +135,41 @@ def execute_job(start_string):
                 # sleep
                 time.sleep(SERVER_POLL_DELAY)
                 # poll the server
-                if should_terminate_job(argv[1]):
+                if should_terminate_job(vm_url, access_key):
                     # kill the job and exit from the loop
                     proc.kill()
                     break
 
 
-def should_terminate_job(location):
-    """ TODO Make request to plone and check if the job should be terminated """
-    return False
+def should_terminate_job(vm_url, access_key):
+
+    response = make_request(vm_url, GET_STATUS_URL, access_key)
+    if not response:
+        logging.error('Was not able to retrieve job status')
+        return False
+    else:
+        pretty = json.loads(response)
+        if pretty.get('message', None):
+            logging.info('response[message]: ' + pretty['message'])
+            return pretty.get('message' != 'Running')
+        else:
+            logging.error('response did not contain field "message"')
+            return False
+        return False
+
+
+def update_job_status(vm_url, access_key, new_status):
+
+    response = make_request(vm_url, UPDATE_STATUS_URL, access_key,
+                            {'new_status': new_status})
+    pretty = json.loads(response)
+
+    if pretty.get('response', None):
+        logging.info('update_job_status response is ' + str(pretty['response']))
+        return pretty['response'] == 'success'
+    else:
+        logging.info('Did not get a valid response from update_job_status')
+        return False
 
 if __name__ == "__main__":
     main()
